e53a4b154f41b712d04c078acc55deecd1a7d047
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-07-30 18:03:30 +0000

    Implement playGame function with integrated playRound logic for 5 rounds
    Defined playGame function to encapsulate the game logic
    Initialized humanScore and computerScore within playGame
    Added playRound function to handle game rules and score updates
    Incorporated a loop in playGame to play 5 rounds by calling playRound
    Updated console logs to display game outcomes and scores

--- foundation/rps/app.js
+++ foundation/rps/app.js
@@ -12,4 +12,30 @@ function getHumanChoice(){
 
 };
 
+function playGame(){
+let humanScore = 0;
+let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    //Rock beats scissors,  scissors beat paper, and paper beats rock.
+        if(humanChoice === computerChoice){
+            console.log("it's a tie");
+        }else if(humanChoice == 'rock' && computerChoice == 'scissor' 
+        || humanChoice == 'scissor' && computerChoice == 'paper'
+        || humanChoice == 'paper' && computerChoice == 'rock'){
+            humanScore++;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`); 
+            console.log(`Your score: = ${humanScore} Computer score ${computerScore}`);
+        }else{
+            computerScore++;
+            console.log(`You lost!!! ${computerChoice} beats ${humanChoice}`); 
+            console.log(`Computer score: = ${computerScore} Your score ${humanScore}`);
+        }
+}
+for(let i = 0; i<5; i++){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+};
+};
+playGame();
