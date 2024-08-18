f6c3d0f40fbf076fca9fb7de7d1b8c0a92090cf0
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-08-18 04:47:28 +0000

    Implement grid creation and random color functionality
    Sets up the grid layout with dynamic color-changing behavior on hover.

--- foundation/etch-a-sketch/app.js
+++ foundation/etch-a-sketch/app.js
@@ -11,3 +11,28 @@ resetButton.classList.add("inputArea");
 resetButton.textContent = "reset Button";
 userChoice.appendChild(resetButton);
 
+const DEFAULT_GRID_X = 16;
+const DEFAULT_GRID_Y = 16;
+
+function createGrid(x,y){
+
+    for(let i = 0; i < x; i++){
+        for(let j = 0; j < y; j++){
+            let div = document.createElement("div");
+            div.classList.add("square");
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = randomColor();
+            });
+            container.appendChild(div);
+        }
+    }
+}
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+  
+createGrid(DEFAULT_GRID_X, DEFAULT_GRID_Y);
