364b823a94d5caab45be6e7b55819ac5e6fbcf82
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-10-21 23:53:40 +0300

    Add book constructor and empty array
    Implements a book constructor function that initializes book objects
    with title, author, pages, and read status.
    Add an info method for formatted book details.

--- full_stack/library/app.js
+++ full_stack/library/app.js
@@ -1,2 +1,13 @@
+const myLibrary = [];
 
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.title},${this.author},${this.pages},${this.read}`
+
+    }
+}
 
