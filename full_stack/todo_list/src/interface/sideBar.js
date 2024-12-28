8a16a46e1d06d31d62ec18ffcf03a629c5acc1ef
mx-99 <125824465+mx-99@users.noreply.github.com>
2024-12-28 18:55:22 +0300

    Add sideBar functions implementation

---
+++ full_stack/todo_list/src/interface/sideBar.js
@@ -0,0 +1,10 @@
+import { displayAddTaskButton } from "../addTask";
+import { displayupdateTaskButton } from "../updateTask";
+import { displayDeleteTaskButton } from "../deleteTask";
+
+export function sideBar() {
+  displayAddTaskButton()
+  displayupdateTaskButton()
+  displayDeleteTaskButton()
+}
+
