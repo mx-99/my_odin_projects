f7c5c8f2e7b516237164b2a0777f30362d054412
mx-99 <125824465+mx-99@users.noreply.github.com>
2025-01-16 13:49:35 +0300

    Add test

--- full_stack/project_hash_map/hashMap.js
+++ full_stack/project_hash_map/hashMap.js
@@ -141,3 +141,32 @@ class HashMap {
     }
 }
 
+const test = new HashMap();
+
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+test.display();
+
+test.set('moon', 'silver');
+test.display();
+
+console.log(test.get('apple'));
+console.log(test.has('banana'));
+console.log(test.remove('carrot'));
+console.log(test.length());
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+test.clear();
+console.log(test.length());
