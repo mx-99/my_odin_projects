62d80328d5226176072d2104ff6fd9a9915b75c2
mx-99 <125824465+mx-99@users.noreply.github.com>
2025-01-16 13:47:35 +0300

    Add resize, set, and get methods to HashMap class
    - Implemented resize method to double the capacity when load factor is exceeded.
    - Added set method to insert or update key-value pairs, with collision handling.
    - Implemented get method to retrieve values by key.

--- full_stack/project_hash_map/hashMap.js
+++ full_stack/project_hash_map/hashMap.js
@@ -14,5 +14,57 @@ class HashMap {
         }
         return Math.abs(hashCode % this.capacity);
     }
+
+    resize() {
+        this.capacity *= 2;
+        const newTable = new Array(this.capacity);
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                for (let [key, value] of this.table[i]) {
+                    const index = this.hash(key);
+                    if (!newTable[index]) {
+                        newTable[index] = [[key, value]];
+                    } else {
+                        newTable[index].push([key, value]);
+                    }
+                }
+            }
+        }
+        this.table = newTable;
+    }
+
+    set(key, value) {
+        if (this.size / this.capacity > this.loadFactor) {
+            this.resize();
+        }
+
+        const index = this.hash(key);
+        const bucket = this.table[index];
+
+        if (!bucket) {
+            this.table[index] = [[key, value]];
+        } else {
+            const existingItem = bucket.find(item => item[0] === key);
+            if (existingItem) {
+                existingItem[1] = value;
+            } else {
+                bucket.push([key, value]);
+            }
+        }
+        this.size++;
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        const bucket = this.table[index];
+
+        if (bucket) {
+            const foundItem = bucket.find(item => item[0] === key);
+            return foundItem ? foundItem[1] : undefined;
+        }
+        return undefined;
+    }
+
+  
 }
 
