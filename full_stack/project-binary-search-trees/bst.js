79296394e1a68209adec07bd7daf15dd4c0e8862
mx-99 <125824465+mx-99@users.noreply.github.com>
2025-01-18 09:17:46 +0300

    Implement buildTree, buildBalancedTree, insert, and insertNode methods

--- full_stack/project-binary-search-trees/bst.js
+++ full_stack/project-binary-search-trees/bst.js
@@ -10,7 +10,45 @@ class Node {
     constructor() {
       this.root = null;
     }
-
+  
+    buildTree(array) {
+      const uniqueSortedArray = [...new Set(array)].sort((a, b) => a - b);
+      this.root = this.buildBalancedTree(uniqueSortedArray);
+    }
+  
+    buildBalancedTree(array) {
+      if (array.length === 0) return null;
+      const mid = Math.floor(array.length / 2);
+      const root = new Node(array[mid]);
+      root.left = this.buildBalancedTree(array.slice(0, mid));
+      root.right = this.buildBalancedTree(array.slice(mid + 1));
+      return root;
+    }
+  
+    insert(value) {
+      const newNode = new Node(value);
+      if (!this.root) {
+        this.root = newNode;
+      } else {
+        this.insertNode(this.root, newNode);
+      }
+    }
+  
+    insertNode(root, newNode) {
+      if (newNode.value < root.value) {
+        if (root.left === null) {
+          root.left = newNode;
+        } else {
+          this.insertNode(root.left, newNode);
+        }
+      } else {
+        if (root.right === null) {
+          root.right = newNode;
+        } else {
+          this.insertNode(root.right, newNode);
+        }
+      }
+    }
   }
   
 
