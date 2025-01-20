2c82c5f860fe3e723e127aec0107f40d8680ac76
mx-99 <125824465+mx-99@users.noreply.github.com>
2025-01-20 19:29:40 +0000

    Update analyzeArray.js

--- full_stack/testing_practice/analyzeArray.js
+++ full_stack/testing_practice/analyzeArray.js
@@ -9,14 +9,4 @@ function analyzeArray(...array){
     }
 }
 
-const object = analyzeArray([1,8,3,4,2,6]);
-
-object == {
-   average: 4,
-   min: 1,
-   max: 8,
-   length: 6
-};
-
-console.log(analyzeArray(1,8,3,4,2,6))
 module.exports = analyzeArray;
